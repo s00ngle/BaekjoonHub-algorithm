c8c3a164920b7b8b4cd2a2dbfd1ae93dc2f618d0
Soongle <42750885+s00ngle@users.noreply.github.com>
2024-07-31 16:36:18 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 732 ms, Memory: 53224 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Main {
 
     static int V, E;
-    static List<Edge>[] graph;
+    static Edge[] graph;
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,61 +14,70 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[V + 1];
-        for (int i = 1; i <= V; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        graph = new Edge[E];
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt((st.nextToken()));
-            graph[start].add(new Edge(end, weight));
-            graph[end].add(new Edge(start, weight));
+            graph[i] = new Edge(start, end, weight);
         }
 
-        System.out.println(prim(1, 0));
+        System.out.println(kruskal());
     }
 
-    static int prim(int start, int weight) {
-        boolean[] visited = new boolean[V + 1];
-        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+    static int kruskal() {
+        Arrays.sort(graph, (o1, o2) -> o1.weight - o2.weight);
 
-        pq.add(new Edge(start, weight));
+        parent = new int[V + 1];
+        for (int i = 1 ; i <= V; i++) {
+            parent[i] = i;
+        }
 
         int result = 0;
-        while (!pq.isEmpty()) {
-            Edge now = pq.poll();
-
-            if(visited[now.to]) {
-                continue;
-            }
-
-            visited[now.to] = true;
-            result += now.weight;
-
-            for (Edge e: graph[now.to]) {
-                if (!visited[e.to]) {
-                    pq.add(e);
-                }
+        for (Edge e: graph) {
+            if (find(e.start) != find(e.end)) {
+                union(e.start, e.end);
+                result += e.weight;
             }
         }
         return result;
     }
 
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a < b) {
+            parent[b] = a;
+        }
+        else {
+            parent[a] = b;
+        }
+    }
+
     static class Edge {
-        int to;
+        int start;
+        int end;
         int weight;
 
-        Edge(int to, int weight) {
-            this.to = to;
+        Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
             this.weight = weight;
         }
 
         @Override
         public String toString() {
-            return "(" + to + ", " + weight + ")";
+            return "(" + start + ", " + end + ", " + weight + ")";
         }
     }
 }

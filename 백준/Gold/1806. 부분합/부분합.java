43c2daaf4259d576dfeef73b84961068d8f04ecc
Soongle <42750885+s00ngle@users.noreply.github.com>
2024-08-08 10:37:05 +0900

    [Gold IV] Title: 부분합, Time: 184 ms, Memory: 22832 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0, end = 0, sum = 0, minLength = Integer.MAX_VALUE;
+
+        while (true) {
+            if (sum >= S) {
+                minLength = Math.min(minLength, end - start);
+                sum -= arr[start++];
+            } else if (end == N) {
+                break;
+            } else {
+                sum += arr[end++];
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(minLength);
+        }
+    }
+}
